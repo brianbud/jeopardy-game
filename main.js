6b7469ad384801d0d90a006dd2cda2f5ab57bf33
Brian Bud <brian@brianbud.com>
2022-06-08 20:49:30 -0400

    create boxes for clues

--- main.js
+++ main.js
@@ -9,5 +9,10 @@ function initializeBoard() {
     let row = document.createElement('div');
     let boxValue = 200 * (i + 1);
     row.className = 'clue-row';
+
+    for (let j = 0; j < 6; j++) {
+      let box = document.createElement('div');
+      box.className = 'clue-box';
+    }
   }
 }
